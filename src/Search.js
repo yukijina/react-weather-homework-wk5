217a2fbedfb58f0e2b2a8c8e54266c2b924e9192
yuki <38411009+yukijina@users.noreply.github.com>
2025-07-04 09:01:48 -0700

    created functions to fetch date and save to the object

--- src/Search.js
+++ src/Search.js
@@ -1,8 +1,64 @@
 import React, { useState } from 'react';
 import './Search.css';
+import axios from 'axios';
 
 export default function Search() {
   const [city, setCity] = useState('New York');
+  const [weatherData, setWeatherData] = useState({});
+
+  function formattedTime(time) {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'Octover',
+      'November',
+      'December',
+    ];
+
+    const days = [
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+      'Sunday',
+    ];
+    const date = new Date(time);
+    // let currentTime = {};
+
+    const currentTime = {
+      month: months[date.getMonth()],
+      date: date.getDate(),
+      day: days[date.getDay()],
+      time: `${date.getHours()}:${date.getMinutes()}`,
+    };
+
+    return currentTime;
+  }
+
+  function showWeather(response) {
+    console.log(response.data);
+    const data = response.data;
+    setWeatherData({
+      city: data.city,
+      country: data.country,
+      description: data.condition.description,
+      temperature: Math.ceil(data.temperature.current),
+      humidity: data.temperature.humidity,
+      wind: data.wind.speed,
+      iconUrl: data.condition.icon_url,
+      time: formattedTime(data.time),
+    });
+    console.log(weatherData);
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -10,11 +66,11 @@ export default function Search() {
 
     const apiKey = '2f5896dd4cc0cdo340203tba4fba205f';
     const url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(url).then(showWeather);
   }
 
   function handleChange(e) {
     e.preventDefault();
-    console.log(e.target.value);
     setCity(e.target.value);
   }
   return (
